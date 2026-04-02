93b30c2330fbab723459308105ce2eb2a54f9a46
DasLampe <daslampe@lano-crew.org>
2026-04-02 22:41:51 +0200

    fix: empty metadata

--- metadata.py
+++ metadata.py
@@ -14,7 +14,7 @@ def add_iptables(metadata):
                 dest_port(metadata.get('wireguard').get(name).get('port')). \
                 comment(f'wireguard {name}')
 
-    for name,conf in metadata.get('wireguard').items():
+    for name,conf in metadata.get('wireguard', {}).items():
         iptables_rules += repo.libs.iptables.accept().chain('INPUT').input(conf.get('interface', name))
         iptables_rules += repo.libs.iptables.accept().chain('OUTPUT').output(conf.get('interface', name))
 
@@ -26,7 +26,7 @@ def backward_compatibility(metadata):
         'wireguard': {},
     }
 
-    for interface,interface_config in metadata.get('wireguard').items():
+    for interface,interface_config in metadata.get('wireguard', {}).items():
         if metadata.get('wireguard').get(interface).get('address', False):
             result['wireguard'][interface] = {
                 'addresses': [metadata.get('wireguard').get(interface).get('address')],
